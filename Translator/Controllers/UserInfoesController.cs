80b9d684e3f0a88660337b3e1d9a0f03dc20d7bd
Peter <youngenli@gmail.com>
2018-08-15 10:18:26 -0500

    initial files

---
+++ Translator/Controllers/UserInfoesController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Translator.Models;
+
+namespace Translator.Controllers
+{
+    public class UserInfoesController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: UserInfoes
+        public ActionResult Index()
+        {
+            var userInfoes = db.UserInfos.Include(u => u.ApplicationUser);
+            return View(userInfoes.ToList());
+        }
+
+        // GET: UserInfoes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserInfo userInfo = db.UserInfos.Find(id);
+            if (userInfo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userInfo);
+        }
+
+        // GET: UserInfoes/Create
+        public ActionResult Create()
+        {
+            ViewBag.Id = new SelectList(db.Users, "Id", "Email");
+            return View();
+        }
+
+        // POST: UserInfoes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "UserId,Id,FirstName,LastName,Email,Ethnicity,Age,Province,Country,LanguagesSpoken,LanguageDesired,FriendId1,FriendId2,FriendId3")] UserInfo userInfo)
+        {
+            
+            if (ModelState.IsValid)
+            {
+                db.UserInfos.Add(userInfo);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Id = new SelectList(db.Users, "Id", "Email", userInfo.Id);
+            return View(userInfo);
+        }
+
+        // GET: UserInfoes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserInfo userInfo = db.UserInfos.Find(id);
+            if (userInfo == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Id = new SelectList(db.Users, "Id", "Email", userInfo.Id);
+            return View(userInfo);
+        }
+
+        // POST: UserInfoes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "UserId,Id,FirstName,LastName,Email,Ethnicity,Age,Province,Country,LanguagesSpoken,LanguageDesired,FriendId1,FriendId2,FriendId3")] UserInfo userInfo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(userInfo).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Id = new SelectList(db.Users, "Id", "Email", userInfo.Id);
+            return View(userInfo);
+        }
+
+        // GET: UserInfoes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserInfo userInfo = db.UserInfos.Find(id);
+            if (userInfo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userInfo);
+        }
+
+        // POST: UserInfoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            UserInfo userInfo = db.UserInfos.Find(id);
+            db.UserInfos.Remove(userInfo);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Translator/Models/UserInfo.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Translator.Models
+{
+    public class UserInfo
+    {
+        [Key]
+        public int UserId { get; set; }
+
+        [ForeignKey("ApplicationUser")]
+        public string Id { get; set; }
+        public virtual ApplicationUser ApplicationUser { get; set; }
+
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Display(Name = "Ethnicity")]
+        public string Ethnicity { get; set; }
+
+        [Display(Name = "Age")]
+        public int Age { get; set; }
+
+        [Display(Name = "Province/State")]
+        public string Province { get; set; }
+
+        [Display(Name = "Country")]
+        public string Country { get; set; }
+
+        [Display(Name = "Languages Known")]
+        public string LanguagesSpoken { get; set; }
+
+        [Display(Name = "Languaged Desired")]
+        public string LanguageDesired { get; set; }
+
+        public int FriendId1 { get; set; }
+
+        public int FriendId2 { get; set; }
+
+        public int FriendId3 { get; set; }
+    }
+}
